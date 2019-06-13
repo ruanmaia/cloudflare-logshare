493138c748ddcfad53c6598a6eb8ddb93926a843
Ruan Maia <ruan.maia@grancursosonline.com.br>
2019-06-13 16:56:02 -0300

    Criação de exportação local

--- app/term.py
+++ app/term.py
@@ -3,30 +3,47 @@ import sys
 import arrow
 import click
 import json
+import shutil
+import os
 
-from app import cli
+from pathlib import Path
+from tqdm import tqdm
+
+from app import cli, config
 
 # IMPORT PONY ORM STATEMENTS
 from pony.orm import *
 
-#### CRAWLER GROUP COMMANDS
+#### CLI COMMANDS
 @cli.command('run')
-@click.option(
-    '--from', '_from', 
-    required=True, type=click.Choice(('local', 's3', 'http-api')), 
-    help='The type of data source'
-)
-@click.option(
-    '--objects-folder',
-    required=True,
-    help='The path to find the log files'
-)
-@click.option(
-    '--to', 'to', 
-    required=True, type=click.Choice(('local', 's3', 'mysql', 'postgresql', 'csv', 'json', 'txt', 'elastic-search', 'http-api')), 
-    help='The type of destination'
-)
-def run(_from, objects_folder, to): 
-    print(_from)
-    print(objects_folder)
-    print(to)
+def run(): 
+    
+    for input_file in tqdm(Path(config['source']['path']).glob('**/*.log.gz')):
+        
+        output_file = str(input_file).replace(config['source']['path'], '').lstrip('/')
+        output_file = Path('/'.join((config['destination']['path'], output_file)))
+
+        os.makedirs(output_file.parent, exist_ok=True)
+        shutil.copyfile(str(input_file), str(output_file))
+
+        os.system('gunzip -fq {}'.format(str(output_file)))
+        adjusted_output = str(output_file.parent) + '/' + str(output_file.stem)
+        lines = open(adjusted_output).readlines()
+
+        # print(adjusted_output)
+
+        with open(adjusted_output, 'w') as f:
+
+            for line in lines:
+                data = json.loads(line)
+                line = '{} {} {} {} {}'.format(
+                    data['EdgeStartTimestamp'],
+                    data['ClientIP'], 
+                    data['ClientRequestHost'], 
+                    data['ClientRequestMethod'], 
+                    data['ClientRequestURI']
+                )
+                # print(line)
+                f.write(line + "\n")
+
+        # exit(0)
